62331b6844863791fd8a57ee3579da236bb42e8f
navid ashraf <ashraf.navid71@gmail.com>
2022-05-12 03:32:06 +0430

    Build sidebar base on user object

--- src/base.js
+++ src/base.js
@@ -7,19 +7,6 @@ import { buildTaskEventListener, buildTaskCheckListener, buildSubTaskCheckListen
 
 export function buildwebsite(){
     document.body.append(sidebarComponent(), projectListComponent(), taskDetailsComponent());
-    for(let i = 2; i < newUser.getProject().length ; i++ ){
-        addProjectSidebar(newUser.getProject()[i][0],newUser.getProject()[i][1]);
-    }
-}
-
-function addProjectSidebar(projectClass,projectName){
-    const newProject = document.createElement('div');
-    newProject.classList.add(projectClass);
-    newProject.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-folder2" viewBox="0 0 16 16">
-    <path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v7a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 12.5v-9zM2.5 3a.5.5 0 0 0-.5.5V6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5zM14 7H2v5.5a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 .5-.5V7z"></path>
-  </svg>
-<p>`+projectName+`</p>`;
-    document.querySelector('.sidebar-projects').insertBefore(newProject,document.querySelector('.new-project-btn'));
 }
 
 //changing project-list interface when clicking on projects in sidebar.
